9661e6f47c77642da5a596587eec3553ebfe682e
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-10-19 22:44:14 -0500

    restore database

--- app/account/api/views.py
+++ app/account/api/views.py
@@ -9,25 +9,25 @@ from account.api import serializers
 
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
-@extend_schema_view(
-    list = extend_schema(description = 'Allow obtain a account list'),
-    retrieve = extend_schema(description = 'Allos obtain a specific account'),
-    create = extend_schema(description = 'Allow create a new account'),
-    update = extend_schema(description = 'Allow update an existing account'),
-    destroy = extend_schema(description = 'Allows delete a account'),
-)
+# @extend_schema_view(
+#     list = extend_schema(description = 'Allow obtain a account list'),
+#     retrieve = extend_schema(description = 'Allos obtain a specific account'),
+#     create = extend_schema(description = 'Allow create a new account'),
+#     update = extend_schema(description = 'Allow update an existing account'),
+#     destroy = extend_schema(description = 'Allows delete a account'),
+# )
 class AccountViewSet(viewsets.ModelViewSet):
     """View for manage account APIs"""
 
     serializer_class = serializers.AccountSerializer
     queryset = Account.objects.all()
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
-    # def get_queryset(self):
-    #     """Retrieve accounts for authenticated user."""
-    #     # return self.queryset.filter(user=self.request.user).order_by('-id')
-    #     return self.queryset.all().order_by('-id')
+    def get_queryset(self):
+        """Retrieve accounts for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+        # return self.queryset.all().order_by('-id')
 
-    # def perform_create(self, serializer):
-    #     """Create a new recipe."""
-    #     serializer.save(user=self.request.user)
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
