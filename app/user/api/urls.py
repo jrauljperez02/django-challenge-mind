58b65a0c95f0fe989fc1811946093ea59ffe75d0
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-10-29 16:14:18 -0500

    Modify views to allow filtering users by name without destroying hash password

--- app/user/api/urls.py
+++ app/user/api/urls.py
@@ -1,15 +1,14 @@
-"""
-URL mappings for the user API.
-"""
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
-from user.api import views
+from user.api.views import UserViewSet
 
+
+router = routers.DefaultRouter()
+router.register('', UserViewSet)
 
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', views.CreateUserView.as_view(), name='create'),
-    # path('token/', views.CreateTokenView.as_view(), name='token'),
-    # path('me/', views.ManageUserView.as_view(), name='me'),
+    path('', include(router.urls)),
 ]
--- app/user/api/views.py
+++ app/user/api/views.py
@@ -1,33 +1,33 @@
 """
-Views for the user API.
+Views for the user API
 """
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from django.contrib.auth import get_user_model
+
 from rest_framework import generics, authentication, permissions
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
+from rest_framework import viewsets
+from user.api.serializers import UserSerializer
 
-from user.api.serializers import (
-    UserSerializer,
-    
-)
+from django_filters import rest_framework as filters
 
-
-class CreateUserView(generics.CreateAPIView):
-    """Create a new user in the system."""
-    serializer_class = UserSerializer
-
-
-# class CreateTokenView(ObtainAuthToken):
-#     """Create a new auth token for user."""
-#     serializer_class = AuthTokenSerializer
-#     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-
-# class ManageUserView(generics.RetrieveUpdateAPIView):
-#     """Manage the authenticated user."""
+# class ManageUserView(generics.RetrieveAPIView):
+#     """Manage the authenticated user"""
 #     serializer_class = UserSerializer
-#     authentication_classes = [authentication.TokenAuthentication]
 #     permission_classes = [permissions.IsAuthenticated]
 
 #     def get_object(self):
-#         """Retrieve and return the authenticated user."""
+#         """Retrieve and return the authenticated user. """
 #         return self.request.user
+        
+class UserFilter(filters.FilterSet):
+    class Meta:
+        model = get_user_model()
+        fields = {
+            'name' : ['icontains']
+        }
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    filterset_class = UserFilter
+
+    permission_classes = [permissions.IsAuthenticated]
